7fc6f0379f35dc42016a66b39cc7ccd3ae26e11f
chumpblocckami <mazzolauni@gmail.com>
2021-03-10 18:00:11 +0100

    added debug errors

--- debug_errors.py
+++ debug_errors.py
@@ -7,3 +7,13 @@ class Error(Exception):
 class UnsupportedFormat(Error):
     """Input is not .json or .csv"""
     pass
+
+class TweepyApiError(Error):
+    """Cannot connect to Twitter API. Please enter credentials"""
+    pass
+class NoCredentialsFound(Error):
+    """Cannot load Twitter API Credentials. Check .env file"""
+    pass
+class StreamError(Error):
+    """Something went wrong during stream processing. To skip this error, set skip_error=True"""
+    pass
